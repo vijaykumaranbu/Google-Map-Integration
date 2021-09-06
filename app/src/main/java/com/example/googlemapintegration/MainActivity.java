b52d51f07662809e326400d87e0bc1a2fc1e992f
vijayakumaranbu <vijayakumaranbu2000@gmail.com>
2021-09-06 17:44:14 +0530

    commit

---
+++ app/src/main/java/com/example/googlemapintegration/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.googlemapintegration;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        LatLng athur = new LatLng(12.736832, 79.941022);
+        map.addMarker(new MarkerOptions().position(athur).title("Athur"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(athur));
+    }
+}
